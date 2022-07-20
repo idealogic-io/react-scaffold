bd4b506c1b747317117ba85c738cb8137ae13e60
Evgeniy Ochkalov <chaba3454@gmail.com>
2022-07-20 09:44:27 +0000

    Update src/services/axios.js

--- src/services/axios.js
+++ src/services/axios.js
@@ -27,7 +27,7 @@ export const getInstance = (baseURL = baseUrl) => {
   instance.interceptors.response.use(
     success => success,
     error => {
-      if (error.response.status === 401) {
+      if (error?.response?.status === 401) {
         store.dispatch(logout());
       }
 
