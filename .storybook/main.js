e5e5adfa219b7227939f31e7b1764788b96433d9
Evgeniy Ochkalov <evgeniy.ochkalov@idealogic.dev>
2022-09-28 10:52:28 +0300

    add spinner, clean storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,13 +7,7 @@ module.exports = {
     return config;
   },
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/preset-create-react-app",
-    "themeprovider-storybook/register",
-  ],
+  addons: ["@storybook/preset-create-react-app"],
   framework: "@storybook/react",
   core: {
     builder: "@storybook/builder-webpack5",
--- src/components/svg/index.ts
+++ src/components/svg/index.ts
@@ -1,2 +1,3 @@
 export { default as Svg } from "./Svg";
 export { default as AddIcon } from "./icons/Add";
+export { default as SpinnerIcon } from "./icons/Spinner";
