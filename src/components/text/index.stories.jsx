8681e8618992c15cfd3efe4b3c2e53b20379a505
Evgeniy Ochkalov <evgeniy.ochkalov@idealogic.dev>
2022-12-05 10:37:34 +0200

    Update with media query

--- src/components/text/index.stories.jsx
+++ src/components/text/index.stories.jsx
@@ -18,11 +18,14 @@ export const Text = () => {
 
       <StyledText color="accent100">Custom color from theme</StyledText>
 
-      <StyledText StyledTextAlign="center">center</StyledText>
-
       <StyledText ellipsis width="100px">
         Ellipsis: a long StyledText with an ellipsis just for the example
       </StyledText>
+
+      <StyledText textAlign="center">center</StyledText>
+
+      <StyledText fontSize={["12px", "16px", "22px"]}>Width media queries in markup as array</StyledText>
+      <StyledText fontSize={{ mobileS: 14, mobileL: 18, tablet: 24 }}>Width media queries in markup as obj</StyledText>
     </div>
   );
 };
--- src/theme/index.ts
+++ src/theme/index.ts
@@ -3,11 +3,11 @@ export { default as dark } from "./dark";
 export { default as light } from "./light";
 export { appearanceAnimationVariants, appearanceAnimationMap } from "./animations";
 
-import { BreakpointsMap, Colors, Fonts, FontWeight, MediaQueries, Radii, Shadows, ZIndices, HSL } from "./types";
+import { Breakpoints, Colors, Fonts, FontWeight, MediaQueries, Radii, Shadows, ZIndices, HSL } from "./types";
 
 export interface CustomTheme {
   siteWidth: number;
-  breakpointMap: BreakpointsMap;
+  breakpoints: Breakpoints;
   mediaQueries: MediaQueries;
   shadows: Shadows;
   radii: Radii;
