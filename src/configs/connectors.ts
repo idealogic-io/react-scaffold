b286dc1324a6f13d7dbb6aafdc1f8cad33d67ce5
Evgeniy Ochkalov <evgeniy.ochkalov@idealogic.dev>
2023-01-06 11:18:56 +0000

    Update src/configs/connectors.ts

--- src/configs/connectors.ts
+++ src/configs/connectors.ts
@@ -4,13 +4,13 @@ import CoinBase from "components/svg/icons/CoinBase";
 
 import { connectorName } from "utils/web3";
 
-const URL = process.env.REACT_APP_URL;
+const URL = process.env.REACT_APP_URL as string;
 
 const connectors = [
   {
     title: "Web3",
     icon: Web3,
-    href: `https://metamask.app.link/dapp/${URL}/`,
+    href: `https://metamask.app.link/dapp/${URL?.replace("https://", "")}/`,
     connectorId: connectorName.injectedConnector,
   },
   {
