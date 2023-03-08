29ecc2521d1fba6d5eed9976b69886b647e597e9
Evgeniy Ochkalov <evgeniy.ochkalov@idealogic.dev>
2023-03-08 08:16:22 +0000

    Update simple-rpc.ts

--- src/utils/web3/simple-rpc.ts
+++ src/utils/web3/simple-rpc.ts
@@ -2,5 +2,5 @@ import { StaticJsonRpcProvider } from "@ethersproject/providers";
 import { rpcUrls } from "configs";
 
 export const getSimpleRpcProvider = (chainId: number) => {
-  return new StaticJsonRpcProvider(rpcUrls[chainId]);
+  return new StaticJsonRpcProvider(rpcUrls[chainId], chainId);
 };
