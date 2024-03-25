6a11665fafd81ea39934cc2ea54bd1e3efcc0106
Evgeniy Ochkalov <evgeniy.ochkalov@idealogic.dev>
2024-03-25 16:58:09 +0100

    Link with full URL

--- src/components/link/index.tsx
+++ src/components/link/index.tsx
@@ -7,6 +7,8 @@ import { getExternalLinkProps } from "components/button";
 // Types
 import { LinkProps } from "./types";
 
+const DOMAIN = process.env.REACT_APP_URL as string;
+
 const Link: React.FC<LinkProps> = ({ external, href, ...props }) => {
   const internalProps = external ? getExternalLinkProps() : {};
   const ariaLabel = props.children && typeof props.children === "string" ? props.children : href || "link";
@@ -15,7 +17,7 @@ const Link: React.FC<LinkProps> = ({ external, href, ...props }) => {
     return <StyledLink as="a" href={href} {...internalProps} {...props} aria-label={ariaLabel} />;
   } else {
     return (
-      <RouterLink to={href || ".."} aria-label={ariaLabel}>
+      <RouterLink to={DOMAIN + (href ?? "")} aria-label={ariaLabel}>
         <StyledLink as="span" {...internalProps} {...props} />
       </RouterLink>
     );
